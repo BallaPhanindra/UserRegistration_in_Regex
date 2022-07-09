86bd11ccf228983778392e7753382ba16ee330bc
Balla <ballaphanindra@outlook.com>
2022-07-10 02:59:00 +0530

    Committing Regex

---
+++ UserRegistration_in_Regex/UserRegistration_in_Regex/Password1_Min8Char.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration_in_Regex
+{
+    internal class Password1_Min8Char
+    {
+        public static string REGEX_Password1 = "^[A-Z a-z]{8,}$"; //Phanindra, ViratKohli
+
+        public bool ValidatePassword1(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password1);
+        }
+    }
+}
